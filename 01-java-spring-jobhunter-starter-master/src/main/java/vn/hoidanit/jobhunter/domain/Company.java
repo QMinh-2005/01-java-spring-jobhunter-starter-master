77c28f31cfc5e9d830775fe302fd4a2e397fb740
QMinh-2005 <quanminh88098@gmail.com>
2026-07-02 21:01:27 +0700

    70

---
+++ 01-java-spring-jobhunter-starter-master/src/main/java/vn/hoidanit/jobhunter/domain/Company.java
@@ -0,0 +1,46 @@
+package vn.hoidanit.jobhunter.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "companies")
+@Getter
+@Setter
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @NotBlank(message = "Tên công ty không được để trống")
+    private String name;
+
+    @Column(columnDefinition = "MEDIUMTEXT")
+    private String description;
+
+    private String address;
+    private String logo;
+
+    private Instant createdAt;
+
+    private Instant updatedAt;
+
+    private String createdBy;
+
+    private String updatedBy;
+
+    @PrePersist
+    public void  BeforeInsert() {
+        this.createdBy = "QuanAnhMinh";
+        this.createdAt = Instant.now();
+    }
+}
